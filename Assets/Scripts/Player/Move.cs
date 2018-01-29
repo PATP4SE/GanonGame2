8255619bfcd96bfd6565477c7ddeeca5555d7897
PATP4SE <p-a_morin@hotmail.com>
2018-01-29 00:02:15 -0500

    Some shits pas imnportant

---
+++ Assets/Scripts/Player/Move.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	private Rigidbody2D playerRig;
+	[SerializeField] public float speed;
+
+	// Use this for initialization
+	void Start () {
+		playerRig = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetAxis ("Horizontal"))
+		{
+			playerRig.MovePosition (new Vector2 (speed, 0));
+		}
+
+
+	}
+}
