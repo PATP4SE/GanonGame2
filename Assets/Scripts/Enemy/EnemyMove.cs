24a26532df9d754fd029962dd0f538a7d2305d87
PATP4SE <p-a_morin@hotmail.com>
2018-02-08 19:26:30 -0500

    Adjust speed + enemy + docs

---
+++ Assets/Scripts/Enemy/EnemyMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour {
+
+	[SerializeField] private float speed;
+	[SerializeField] private float decelerationPercentage;
+	[SerializeField] private GameObject player;
+
+	private Rigidbody2D rigidBody;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Utils.DecelerateX(ref rigidBody, decelerationPercentage);
+
+		float posX = (player.transform.position.x - rigidBody.transform.position.x) > 0 ? 1 : -1;
+
+		rigidBody.AddForce(new Vector3 ( posX * speed, 0), ForceMode2D.Impulse);
+	}
+}
--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -2,15 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Move : MonoBehaviour {
+public class PlayerMove : MonoBehaviour {
 
 
-	[SerializeField] public float speed;
-	[SerializeField] public float jumpSpeed;
+	[SerializeField] private float speed;
+	[SerializeField] private float jumpSpeed;
+	[SerializeField] private float decelerationPercentage;
 
 	private Rigidbody2D playerRig;
 	private SpriteRenderer playerSprite;
-	private BoxCollider2D playerCollider;
+	//private BoxCollider2D playerCollider;
 	private Animator playerAnimator;
 
 	private bool isJumping = false;
@@ -20,22 +21,24 @@ public class Move : MonoBehaviour {
 		playerRig = GetComponent<Rigidbody2D> ();
 		playerSprite = GetComponent<SpriteRenderer> ();
 		playerAnimator = GetComponent<Animator> ();
-		playerCollider = GetComponent<BoxCollider2D> ();
+		//playerCollider = GetComponent<BoxCollider2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
+		Utils.DecelerateX(ref playerRig, decelerationPercentage);
+
 		if (Input.GetKey (KeyCode.D)) 
 		{
 			playerSprite.flipX = false;
-			playerRig.AddForce (new Vector2 (speed, 0));
+			playerRig.AddForce (new Vector2 (speed, 0), ForceMode2D.Impulse);
 			playerAnimator.SetBool ("isWalking", true);
 		} 
 		else if (Input.GetKey (KeyCode.A)) 
 		{
 			playerSprite.flipX = true;
-			playerRig.AddForce (new Vector2 (-speed, 0));
+			playerRig.AddForce (new Vector2 (-speed, 0), ForceMode2D.Impulse);
 			playerAnimator.SetBool ("isWalking", true);
 		} 
 		else 
@@ -52,14 +55,35 @@ public class Move : MonoBehaviour {
 			}
 		}
 
+
+		if (playerAnimator.GetCurrentAnimatorStateInfo (0).IsName ("Die")) 
+		{
+		}
+
+
 	}
 
 	void OnCollisionEnter2D(Collision2D coll) {
 		isJumping = false;
+
+		if (coll.gameObject.tag == "Enemy") 
+		{
+			die ();
+		}
 
 		//if (coll.gameObject.tag == "Enemy")
 		//	coll.gameObject.SendMessage("ApplyDamage", 10);
 
 	}
 
+
+	private void die()
+	{
+		playerAnimator.SetBool ("isDead", true);
+
+		//yield return new WaitForSeconds(playerAnimator.GetCurrentAnimatorStateInfo(0).length + playerAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime);
+
+		Destroy(this.gameObject);
+	}
 }
+
